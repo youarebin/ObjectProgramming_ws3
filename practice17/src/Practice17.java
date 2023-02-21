78e622cbae522da5a2c5ae1536163fb9a1b4335b
yubinka <kayubin27@gmail.com>
2023-02-21 21:15:04 +0900

    first commit

---
+++ practice17/src/Practice17.java
@@ -0,0 +1,20 @@
+//컴퓨터학과_가유빈_20220740
+public class Practice17 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for(int i = 1; i < 11; i++)
+		{
+			for(int j = 1; j < 11; j++)
+			{
+				if(j < 3)
+					System.out.print(i * j + "\t");
+				else
+					System.out.print(i * j + "\t");
+			}
+			System.out.println();
+		}
+	}
+
+}
