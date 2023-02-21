14ec044a7cd797aae2ba9738943c91c184e3584d
yubinka <kayubin27@gmail.com>
2023-02-21 21:15:20 +0900

    first commit

---
+++ practice18/src/Practice18.java
@@ -0,0 +1,26 @@
+//컴퓨터학과_가유빈_20220740
+public class Practice18 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for(int i = 1; i < 10; i++)
+		{
+			for(int j = 2; j < 6; j++)
+			{
+				System.out.print(j + " * " + i + " = " + i*j + "\t");
+			}
+			System.out.print("\n");
+		}
+		System.out.print("\n");
+		for(int i = 1; i < 10; i++)
+		{
+			for(int j = 6; j < 10; j++)
+			{
+				System.out.print(j + " * " + i + " = " + i*j + "\t");
+			}
+			System.out.print("\n");
+		}
+	}
+
+}
